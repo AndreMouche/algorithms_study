2d0d8c5e019af95c9442fbdc0a8ac744a44a7e0a
AndreMouche <AndreMouche@126.com>
2012-08-07 00:55:26 +0800

    add examples and exercises for class 2.1

---
+++ chapter02/example/2.1_insetion_sort.cpp
@@ -0,0 +1,70 @@
+/**
+ * Insertion sort
+ * Time complexity:O(n^2)
+ * Page:10
+ *
+ * Description:
+ *  使用插入排序法对n个整数进行排序
+ * input:
+ * n   ...indicate the number of integers to sort
+ * n integers     ..the numbers need to sort
+ *
+ * output:
+ * n integers for each case,in nondecreasing order
+ *
+ * examples:
+ * input:
+	5
+	5 4 3 2 1
+	6
+	9 6 3 6 3 1
+
+   output:
+   1 2 3 4 5
+   1 3 3 6 6 9
+ *
+ */
+
+#include<stdio.h>
+#include<string.h>
+const int N = 10000;
+
+/**
+对有n个元素的整数数组number进行插入排序
+复杂度为O(n^2)
+@param n: the length of the array
+@number:the array need to sort
+*/
+void insertion_sort( int n,int *number ) {
+    int i,j;
+	for(i=1;i<n;i++) {
+	   j = i-1;
+	   int key = number[i];
+	   while(j>=0&&number[j]>key) {
+	      number[j+1]=number[j];
+		  j--;
+	   }
+	  number[j+1] = key;
+	}
+}
+
+
+int main()
+{
+	int n;
+	int a[N];
+	while(scanf("%d",&n)!=EOF) {
+	   int i;
+	   for(i=0;i<n;i++) {
+	     scanf("%d",&a[i]);
+	   }
+	   insertion_sort(n,a);
+	   for(i=0;i<n;i++) {
+	       printf("%d ",a[i]);
+	   }
+	   printf("\n");
+	}
+	return 0;
+}
+
+
---
+++ chapter02/exercise/2.1_4_base_sum.cpp
@@ -0,0 +1,76 @@
+/**
+ * 练习 2.1-4
+ * Page:19
+ * 描述：有两个各自存放在数组A和数组B中的n位二进制整数，考虑它们的相加问题。
+ * 变形后，本程序的实现，输入：
+ * 每个case输入三行数据，其中第一行为一个在[2,10]内的一个整数base，第二三行代表base进制的数字
+ * 输出：求第二、三行所代表数字的和，并将结果以base进制的形式呈现
+ *
+ */
+#include<stdio.h>
+#include<string.h>
+const int N = 10000;
+/**
+ * 数组a,b分别代表alength,blength位的base进制数，其中数组的每个数代表一位
+ * 求以上两个base进制数的和，为求方便，其中base <= 10
+ * @param 字符串 a,数组a,a[i]代表该位数的数值
+ * @param 字符串 b,类似数组a，代表一个base进制的数字
+ * 求以上两数的和
+*/
+void sum(char *a,char *b,int base) {
+   int ans[N];
+   int i = 0;
+   memset(ans,0,sizeof(ans));
+   int alength = strlen(a);
+   int blength = strlen(b);
+   alength --;
+   blength --;
+   while(alength >= 0 && blength >=0) {
+	   ans[i]+=a[alength]-'0' + b[blength]-'0';
+	   ans[i+1] = ans[i+1] +  ans[i]/base;
+	   ans[i] %= base;
+	   i++;
+	   alength --;
+	   blength --;
+   }
+
+   while(alength >= 0) {
+	   ans[i]+=a[alength]-'0';
+	   ans[i+1] = ans[i+1] + ans[i]/base;
+	   ans[i] %= base;
+	   i++;
+	   alength --;
+   }
+
+   while(blength >= 0) {
+	   ans[i] += b[blength]-'0';
+	   ans[i+1] = ans[i+1] + ans[i]/base;
+	   ans[i] %= base;
+	   i++;
+	   blength --;
+   }
+
+   while(a[i]>base) {
+	   ans[i+1] = ans[i+1]+a[i]/base;
+	   ans[i] %= base;
+	   i++;
+   }
+
+   while(i>0&&ans[i]==0)i--;
+   while(i>=0){
+	   printf("%d",ans[i]);
+       i--;
+   }
+   printf("\n");
+}
+
+int main()
+{
+	char a[N],b[N];
+	int base;
+	while(scanf("%d",&base)!=EOF) {
+		scanf("%s%s",a,b);
+		sum(a,b,base);
+	}
+	return 0;
+}
