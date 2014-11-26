25a034e257ff4fa72077c1162ae7ec6b15fd2500
AndreMouche <AndreMouche@126.com>
2014-11-26 21:53:31 +0800

    update annotation

--- leetcode/README.md
+++ leetcode/README.md
@@ -1,9 +1,4 @@
-[word-break](http://oj.leetcode.com/problems/word-break/)
-DP
+[word-break](http://oj.leetcode.com/problems/word-break/) 给定一个字典和字符串，判断该字符串能否由字典里面的多个单词组成。很明显的DP，定义一个bool型的DP，dp[i]＝true 表示包括第i个字符在内的s前缀能由字典中的单词组成。复杂度O（N^2)
 
-```
- bool dp[s.length()]
- dp[i]＝true 表示包括第i个字符在内的s前缀能由字典中的单词组成
-```
 
 
--- leetcode/word_break.cpp
+++ leetcode/word_break.cpp
@@ -1,11 +1,9 @@
 /*
  * https://oj.leetcode.com/problems/word-break/
- * 设计dp[]
- * dp[i]＝true 表示包括第i个字符在内的s前缀能由字典中的单词组成
- * 那么dp[0]=true;
- * dp[j] = f{dp[i-1]&dict.find(s.substr(i-1,j-i+1))}
- * j>=i
- * 中间有一个i满足条件即可
+ * 给定一个字典和字符串，判断该字符串能否由字典里面的多个单词组成。
+ * 很明显的DP，定义一个bool型的DP
+ * dp[i]＝true 表示包括第i个字符在内的s前缀能由字典中的单词组成。
+ * 复杂度O（N^2)
  * */
 
 class Solution {
