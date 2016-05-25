ed7bf98559b37f42b369fd04c67c0702e6ec8548
wuxuelian <wuxuelian@trthi.com>
2016-05-25 20:53:57 +0800

    init 263

---
+++ leetcode/263_UglyNumber.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public boolean isUgly(int num) {
+        if(num<1){
+            return false;
+        }
+        while(num!=1){
+            if(num%2==0){
+                num/=2;
+                continue;
+            }
+            if(num%3==0){
+                num/=3;
+                continue;
+            }
+            if(num%5==0){
+                num/=5;
+                continue;
+            }
+            return false;
+        }
+        return true;
+    }
+}
