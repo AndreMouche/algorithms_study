6380060acd577f128a9c1e85f573c4e25000ec3e
shirly <AndreMouche@126.com>
2022-08-24 10:55:20 +0800

    init set_zero

--- leetcode/rust/leetcode/src/solutions/set_zero.rs
+++ leetcode/rust/leetcode/src/solutions/set_zero.rs
@@ -2,45 +2,41 @@ use std::collections::HashSet;
 
 pub struct Solution {}
 impl Solution {
+    /**
+     * https://leetcode.cn/problems/set-matrix-zeroes/
+     * 简单题
+     */
     pub fn set_zeroes(matrix: &mut Vec<Vec<i32>>) {
         if matrix.is_empty() {
             return;
         }
-        let mut rows = vec![];
-        let mut left_rows = vec![];
-        let mut cols = HashSet::new();
         let rows_len = matrix.len();
         let cols_len = matrix[0].len();
-        for r in 0..rows_len {
-            let mut zero_r = false;
-            for c in 0..cols_len {
+        let first_row_contains_zero = (matrix[0].iter()).any(|c| *c == 0);
+        let first_cell_contains_zero = (0..rows_len).any(|r| matrix[r][0] == 0);
+        for r in 1..rows_len {
+            for c in 1..cols_len {
                 if matrix[r][c] == 0 {
-                    zero_r = true;
-                    cols.insert(c);
+                    if matrix[r][0] != 0 {
+                        for c_id in 0..c {
+                            matrix[r][c_id] = 0;
+                        }
+                    }
+                    if matrix[0][c] != 0 {
+                        for r_id in 0..r {
+                            matrix[r_id][c] = 0;
+                        }
+                    }
+                } else if matrix[0][c] == 0 || matrix[r][0] == 0 {
+                    matrix[r][c] = 0
                 }
             }
-            if !zero_r {
-                left_rows.push(r);
-            } else {
-                rows.push(r);
-            }
         }
-        if rows.is_empty() {
-            return;
+        if first_row_contains_zero {
+            (0..cols_len).for_each(|c| matrix[0][c] = 0);
         }
-        // set rows
-        rows.iter().for_each(|r| {
-            for c in 0..cols_len {
-                matrix[*r][c] = 0;
-            }
-        });
-        if left_rows.is_empty() {
-            return;
+        if first_cell_contains_zero {
+            (0..rows_len).for_each(|r| matrix[r][0] = 0);
         }
-        cols.iter().for_each(|c| {
-            left_rows.iter().for_each(|r| {
-                matrix[*r][*c] = 0;
-            });
-        });
     }
 }
